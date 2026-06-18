ba3a3230e5e70667946740c96a229e7c5496c171
Axel Marco <axel_davues@estudante.sesisenai.org.br>
2026-06-17 21:41:07 -0300

    Update and rename NewInputMove.cs to NewInputMove&Jump.cs

--- NewInputMove&Jump.cs
+++ NewInputMove&Jump.cs
@@ -6,22 +6,37 @@ public class PlayerMove : MonoBehaviour
     public InputAction playerControls;
     public Rigidbody2D playerPhysics;
     public float playerSpeed = 5f;
+    public InputAction PlayerJump;
+    public float playerJumpForce;
+    public static int playerCoins;
     Vector2 playerDirection;
     private void OnEnable()
     {
         
         playerControls.Enable();
+        PlayerJump.Enable();
+        PlayerJump.performed += DoJump;
 
     }
     private void OnDisable()
     {
         
         playerControls.Disable();
-
+        PlayerJump.Disable();
     }
     void Update()
     {
         playerDirection = playerControls.ReadValue<Vector2>();
         playerPhysics.linearVelocity = new Vector2(playerDirection.x * playerSpeed , playerPhysics.linearVelocity.y);
     }
+
+    public void DoJump(InputAction.CallbackContext context)
+    {
+        
+         playerPhysics.linearVelocity = new Vector2(playerPhysics.linearVelocity.x, playerJumpForce);
+
+    }
+
+    
+
 }
